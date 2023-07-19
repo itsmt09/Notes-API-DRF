f56736b74534a63900bacc606c7e4e90d7f5336c
Manish Tekam <manish.tekam.@tejgyan.org>
2023-07-19 23:18:49 +0530

    added status choices

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 class Note(models.Model):
+
+    ARC = 'ARCHIVED'
+    DRA = 'DRAFT'
+    PUB = 'PUBLISHED'
+
+    status_choices = [
+        (ARC, 'Archived'),
+        (DRA, 'Draft'),
+        (PUB, 'Published')
+    ]
     title = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices = status_choices, max_length = 10, default=DRA)
 
     def __str__(self):
         return str(self.title)
--- pages/serializers.py
+++ pages/serializers.py
@@ -5,4 +5,4 @@ class NoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Note
-        fields = ['title', 'description']
+        fields = ['title', 'description', 'status']
